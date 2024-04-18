567e4af83100322f90b684ef1b053364de104e01
Owen Fujii <dalekrebel@gmail.com>
2024-04-18 08:57:58 -0600

    Added Header and Options

--- Stan'sGrocery/StansGrocery.vb
+++ Stan'sGrocery/StansGrocery.vb
@@ -1,3 +1,15 @@
-﻿Public Class StansGrocery
+﻿'Owen Fujii
+'RCET 2265
+'Spring 2024
+'Stan's Grocery
+'https://github.com/Masaharu41/Stan-sGrocery.git
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+
+
+Public Class StansGrocery
+    Dim groceryItems As New List(Of String)
 
 End Class
