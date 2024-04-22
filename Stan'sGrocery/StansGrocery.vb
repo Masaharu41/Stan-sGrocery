e8a450f8b6bcf7b4cc9014fe67495770f88c9ca2
Owen Fujii <dalekrebel@gmail.com>
2024-04-22 09:00:40 -0600

    Cleaned Code Need To Remove Delimiters

--- Stan'sGrocery/StansGrocery.vb
+++ Stan'sGrocery/StansGrocery.vb
@@ -14,28 +14,15 @@ Public Class StansGrocery
     Dim food(2, 500) As String
 
     Private Sub Loader(sender As Object, e As EventArgs) Handles Me.Load
-        'RowIndex()
+
         CreateFoodArray()
     End Sub
 
-    Function RowIndex() As Integer
-        Dim i As Integer = 0
-        Try
-            FileOpen(1, "..\..\Grocery.txt", OpenMode.Input)
-        Catch ex As Exception
-            FileOpen(2, "..\..\ErrorLog.txt", OpenMode.Append)
-            Write(2, CStr($"Error: {Err.Number}, {Err.Description} {vbNewLine}"))
-            FileClose(2)
-        End Try
-        Do Until EOF(1)
-            i = i + 1
-        Loop
-        Return i
-    End Function
+
     Sub CreateFoodArray()
         Dim temp() As String
         Dim groceryItems As String
-        ' Dim tempGlobArray(2, 500) As String
+
         Dim i As Integer = 0
         Try
             FileOpen(1, "..\..\Grocery.txt", OpenMode.Input)
@@ -51,27 +38,28 @@ Public Class StansGrocery
             food(0, i) = temp(0)
             food(1, i) = temp(1)
             food(2, i) = temp(2)
-            'tempGlobArray(0, i) = temp(0)
-            'tempGlobArray(1, i) = temp(1)
-            'tempGlobArray(2, i) = temp(2)
             i = i + 1
         Loop
         ReDim Preserve food(2, i - 1)
-        ' ReDim Preserve tempGlobArray(2, i - 1)
-        'food(,) = tempGlobArray(,)
-        ' SearchTextBox.Text = food(2, 1)
+
         FileClose(1)
     End Sub
 
     Function SearchByName() As String
-        ' Dim temp() As String
-        ' Dim e As Integer = 0
-        Dim itemName$
-        'Dim endofArray As Integer
+
+        Dim itemName As String
+        Dim userInput As String
 
         For i = 0 To UBound(food, 2)
 
             itemName = food(0, i)
+            '  itemName = Split(itemName, "$$ITM")
+            userInput = "$$ITM" & SearchTextBox.Text
+            If itemName = userInput Then
+                MsgBox("We found your Item")
+            Else
+
+            End If
             DisplayListBox.Items.Add(itemName)
 
 
